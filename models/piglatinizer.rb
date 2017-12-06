3a57516b796f39f4653b59c48e2430f1d742e0e2
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-06 13:46:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,22 @@
 class PigLatinizer
 
     def piglatinize(word)
-        
+
         if /[aeiou]/ === word[0].downcase
             "#{word}way"
-        else
-            first = word.split(/[aeiou]/).first
-            word = word.sub(first,"")
-            "#{word}#{first}ay"
-        end
+        # else
+        #     first = word.split(/[aeiou]/).first
+        #     second = word.sub(first,"")
+        #     "#{second}#{first}ay"
+        # end
+      else
+        word.rotate until /[aeiou]/ === word[0].downcase
+        
+      end
     end
 
     def to_pig_latin(sentence)
-        sentence.split(" ").collect {|word| piglatinize(word)}.join(" ")    
+        sentence.split(" ").collect {|word| piglatinize(word)}.join(" ")
     end
 
 end
